c9f094206c2d109a1e666867174519699b6a2621
grhambarde46 <grhambarde46@gmail.com>
2022-11-14 22:45:23 +0530

    My first commit

---
+++ ArrayProject/src/com/java/ArrayProject.java
@@ -0,0 +1,16 @@
+package com.java;
+
+import java.util.ArrayList;
+
+public class ArrayProject {
+
+	public static void main(String[] args) {
+		ArrayList arraylist = new ArrayList();
+		arraylist.add(10);
+		arraylist.add(12);
+		arraylist.add(15);
+		arraylist.add(20);
+		System.out.println(arraylist);
+	}
+
+}
